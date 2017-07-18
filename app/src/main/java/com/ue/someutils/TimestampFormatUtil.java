64f269aad12cd8c8d4a00293d910f8bdc4b1685b
tom <120276969@qq.com>
2017-07-18 13:01:12 +0800

    update

--- app/src/main/java/com/ue/someutils/TimestampFormatUtil.java
+++ app/src/main/java/com/ue/someutils/TimestampFormatUtil.java
@@ -43,38 +43,4 @@ public class TimestampFormatUtil {
 
         return new SimpleDateFormat("y年M月d日", Locale.CHINA).format(new Date(timestamp));
     }
-
-    public static String formatApproximately1(long timestamp) {
-        long dTime = System.currentTimeMillis() - timestamp;
-        if (dTime < 0) {
-            throw new IllegalArgumentException();
-        }
-        int dSecond = (int) ((dTime + 999) / 1000);
-        if (dSecond < 60) {
-            return "刚刚";
-        }
-        int dMinute = (dSecond + 59) / 60;
-        if (dMinute < 60) {
-            return String.format("%d分钟前", dMinute);
-        }
-        int dHour = (dMinute + 59) / 60;
-        if (dHour < 24) {
-            return String.format("%d小时前", dHour);
-        }
-        int dDay = (dHour + 23) / 24;
-        if (dDay == 1) {
-            return "昨天";
-        }
-        if (dDay < 30) {
-            return String.format("%d天前", dDay - 1);
-        }
-        int dMonth = (dDay + 29) / 30;
-        if (dMonth < 12) {
-            Calendar calendar = Calendar.getInstance(Locale.CHINA);
-            calendar.setTimeInMillis(timestamp);
-            return new SimpleDateFormat("M月d日", Locale.CHINA).format(calendar.getTime());
-        }
-
-        return new SimpleDateFormat("y年\nM月d日", Locale.CHINA).format(new Date(timestamp));
-    }
 }
